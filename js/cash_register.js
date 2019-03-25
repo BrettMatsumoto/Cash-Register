b7cba12274fb5a86b390d5d888083cc283bfc144
Brett <brett_matsumoto@yahoo.com>
2019-03-25 02:33:38 -1000

    added sub,mult,divide eventlisteners

--- js/cash_register.js
+++ js/cash_register.js
@@ -2,15 +2,15 @@
 //declare variables
 
 var display = document.getElementById('display');
-var hiddNum = Number(calcMod.getTotal()); //number used to do calculations behind the scene
 var numButts = document.getElementsByClassName('numbers');
 var opButts = document.getElementsByClassName('operator');
 var calcMod = calculatorModule();
-var dispNum = Number(display.innerHTML); //number to be displayed as an equation
+var dispNum = Number(display.innerHTML); //displayed str to number
 
 //set display to total
 
-display.innerHTML = calcMod.getTotal();
+display.innerHTML = 0;
+// console.log(typeof display.innerHTML) *string*
 
 //add eventlistener to numbers
 
@@ -24,23 +24,56 @@ function newDisp(){
     } else {
         display.innerHTML += calcMod.load(toNum);
     }
-
-
-}
-
-//add eventlistener to operators to display operation
-
-for(var i = 0; i<opButts.length;i++){
-    opButts[i].addEventListener('click', incOp)
-}
-function incOp(){
-    display.innerHTML += this.innerHTML
 }
 
 //add eventlistener to operators to calculate
 
 var addition = opButts[0];
-addition.addEventListener('click',toCalc)
-function toCalc(){
-    calcMod.add(dispNum)
+addition.addEventListener('click',toCalcAdd)
+function toCalcAdd(){
+    calcMod.add(dispNum);
+    console.log(calcMod.add(dispNum))
+    display.innerHTML = 0;
 }
+
+var subtraction = opButts[1];
+subtraction.addEventListener('click',toCalcSub)
+function toCalcSub(){
+    calcMod.subtract(dispNum);
+    display.innerHTML = 0;
+}
+
+var multiplication = opButts[2];
+multiplication.addEventListener('click',toCalcMult)
+function toCalcMult(){
+    calcMod.multiply(dispNum);
+    display.innerHTML = 0;
+}
+
+var clear = opButts[3];
+
+var equals = opButts[4];
+equals.addEventListener('click',giveSol)
+function giveSol(){
+    display.innerHTML = calcMod.getTotal()
+    console.log(display.innerHTML)
+}
+
+var division = opButts[5];
+division.addEventListener('click',toCalcDiv);
+function toCalcDiv(){
+    calcMod.divide(dispNum);
+    display.innerHTML = 0;
+}
+
+//to use later... maybe
+
+//add eventlistener to operators to display operation
+
+// for(var i = 0; i<opButts.length;i++){
+//     opButts[i].addEventListener('click', incOp)
+//     console.log(opButts[i])
+// }
+// function incOp(){
+//     display.innerHTML += this.innerHTML
+// }
