fe0874f78726c106009b47cdc19697e0e1ca55cd
Brett <brett_matsumoto@yahoo.com>
2019-03-24 19:52:21 -1000

    buttons show on display

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,7 +1,23 @@
 //declare variables
 var display = document.getElementById('display')
-var buttons = document.getElementsByClassName('numbers');
+var numButts = document.getElementsByClassName('numbers');
+var opButts = document.getElementsByClassName('operator')
 //set display to total
 display.innerHTML = calculatorModule().getTotal();
 
-calculatorModule().load(1)
+//add eventlistener to numbers
+for (var i = 0;i<numButts.length;i++){
+    numButts[i].addEventListener('click', newDisp)
+}
+function newDisp(){
+    var toNum = Number(this.innerHTML)
+    display.innerHTML += calculatorModule().load(toNum)
+    console.log(this.innerHTML)
+}
+//add eventlistener to operators
+for(var i = 0; i<opButts.length;i++){
+    opButts[i].addEventListener('click', incOp)
+}
+function incOp(){
+    display.innerHTML += this.innerHTML
+}
