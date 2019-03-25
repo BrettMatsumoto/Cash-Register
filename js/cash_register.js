674847be0b570e024505295f17ac495df178f3d3
Brett <brett_matsumoto@yahoo.com>
2019-03-25 01:05:40 -1000

    added calcMod var

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,9 +1,10 @@
 //declare variables
 var display = document.getElementById('display')
 var numButts = document.getElementsByClassName('numbers');
-var opButts = document.getElementsByClassName('operator')
+var opButts = document.getElementsByClassName('operator');
+var calcMod = calculatorModule();
 //set display to total
-display.innerHTML = calculatorModule().getTotal();
+display.innerHTML = calcMod.getTotal();
 
 //add eventlistener to numbers
 for (var i = 0;i<numButts.length;i++){
@@ -12,15 +13,21 @@ for (var i = 0;i<numButts.length;i++){
 function newDisp(){
     var toNum = Number(this.innerHTML)
     if(display.innerHTML === '0'){
-        display.innerHTML = calculatorModule().load(toNum);
+        display.innerHTML = calcMod.load(toNum);
     } else {
-        display.innerHTML += calculatorModule().load(toNum);
+        display.innerHTML += calcMod.load(toNum);
     }
 }
-//add eventlistener to operators
+//add eventlistener to operators to display operation
 for(var i = 0; i<opButts.length;i++){
     opButts[i].addEventListener('click', incOp)
 }
 function incOp(){
     display.innerHTML += this.innerHTML
+}
+//add eventlistener to operators to calculate
+var addition = opButts[0];
+addition.addEventListener('click',toCalc)
+function toCalc(){
+
 }
