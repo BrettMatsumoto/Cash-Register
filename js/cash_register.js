a74fdfc495f6359bccb3fe033e2672d56ba02a25
Brett <brett_matsumoto@yahoo.com>
2019-03-25 01:19:59 -1000

    created hiddNum variable and added comments

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,12 +1,19 @@
+
 //declare variables
-var display = document.getElementById('display')
+
+var display = document.getElementById('display');
+var hiddNum = Number(calcMod.getTotal()); //number used to do calculations behind the scene
 var numButts = document.getElementsByClassName('numbers');
 var opButts = document.getElementsByClassName('operator');
 var calcMod = calculatorModule();
+var dispNum = Number(display.innerHTML); //number to be displayed as an equation
+
 //set display to total
+
 display.innerHTML = calcMod.getTotal();
 
 //add eventlistener to numbers
+
 for (var i = 0;i<numButts.length;i++){
     numButts[i].addEventListener('click', newDisp)
 }
@@ -17,17 +24,23 @@ function newDisp(){
     } else {
         display.innerHTML += calcMod.load(toNum);
     }
+
+
 }
+
 //add eventlistener to operators to display operation
+
 for(var i = 0; i<opButts.length;i++){
     opButts[i].addEventListener('click', incOp)
 }
 function incOp(){
     display.innerHTML += this.innerHTML
 }
+
 //add eventlistener to operators to calculate
+
 var addition = opButts[0];
 addition.addEventListener('click',toCalc)
 function toCalc(){
-
+    calcMod.add(dispNum)
 }
