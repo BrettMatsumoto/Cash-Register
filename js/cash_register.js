6153b165865a64aec8ec7862f7a3b2981b9e9539
Brett <brett_matsumoto@yahoo.com>
2019-03-27 14:01:45 -1000

    added some secrets

--- js/cash_register.js
+++ js/cash_register.js
@@ -13,7 +13,72 @@ var clicked = false;
 for (var i = 0; i < numButts.length; i++){
     numButts[i].addEventListener('click',setDisp)
 }
-
+// add eventlisteners special menu items
+for (var i = 0; i < specItems.length; i++){
+    specItems[i].addEventListener('click', setDisp2)
+}
+// add eventlisteners to operator buttons
+opButt[0].addEventListener('click',additionOp)
+function additionOp(){
+    lastNum = Number(disp.innerHTML)
+    operatorChoice = '+'
+    clicked = true;
+    console.log(lastNum)
+}
+opButt[1].addEventListener('click',subtractionOp)
+function subtractionOp(){
+    lastNum = Number(disp.innerHTML)
+    operatorChoice = '-'
+    clicked = true;
+}
+opButt[2].addEventListener('click',multiplyOp)
+function multiplyOp(){
+    lastNum = Number(disp.innerHTML)
+    operatorChoice = '*'
+    clicked = true;
+}
+opButt[3].addEventListener('click',clearOp)
+function clearOp(){
+    disp.innerHTML = "";
+}
+opButt[5].addEventListener('click',divideOp)
+function divideOp(){
+    lastNum = Number(disp.innerHTML);
+    operatorChoice = '/';
+    clicked = true;
+}
+opButt[4].addEventListener('click',getTotal)
+function getTotal(){
+    if (operatorChoice == '+'){
+        var res = Number(lastNum) + Number(disp.innerHTML);
+        disp.innerHTML = res;
+        if (res == 17){
+            alert("FOUND SECRET 2 OF 17!")
+        }
+    } else if (operatorChoice == '-'){
+        var res = Number(lastNum) - Number(disp.innerHTML);
+        disp.innerHTML = res;
+        if (res == 17){
+            alert('FOUND SECRET 3 OF 17!')
+        }
+    } else if (operatorChoice == '*'){
+        var res = Number(lastNum) * Number(disp.innerHTML);
+        disp.innerHTML = res;
+        if (res == 17){
+            alert('FOUND SECRET 4 OF 17!')
+        }
+    } else if (operatorChoice == '/'){
+        var res = Number(lastNum) / Number(disp.innerHTML);
+        disp.innerHTML = res;
+        if (res == 17){
+            alert('FOUND SECRET 5 OF 17')
+        }
+    } else {
+        throw error
+    }
+    operatorChoice = null;
+}
+// set disply after number push;
 function setDisp(){
     if (disp.innerHTML.length < 1){
         disp.innerHTML = this.innerHTML;
@@ -24,12 +89,7 @@ function setDisp(){
         disp.innerHTML += this.innerHTML;
     }
 }
-// add eventlisteners special menu items
-
-for (var i = 0; i < specItems.length; i++){
-    specItems[i].addEventListener('click', setDisp2)
-}
-
+// set display after operator push;
 function setDisp2(){
     if (disp.innerHTML.length < 1){
         disp.innerHTML = this.value
@@ -40,57 +100,18 @@ function setDisp2(){
     }
 }
 
-// add eventlisteners to operator buttons
-opButt[0].addEventListener('click',additionOp)
-function additionOp(){
-    lastNum = Number(disp.innerHTML)
-    operatorChoice = '+'
-    clicked = true;
-    console.log(lastNum)
-}
+//secret display button
 
-opButt[1].addEventListener('click',subtractionOp)
-function subtractionOp(){
-    lastNum = Number(disp.innerHTML)
-    operatorChoice = '-'
-    clicked = true;
-}
-
-opButt[2].addEventListener('click',multiplyOp)
-function multiplyOp(){
-    lastNum = Number(disp.innerHTML)
-    operatorChoice = '*'
-    clicked = true;
-}
-
-opButt[5].addEventListener('click',divideOp)
-function divideOp(){
-    lastNum = Number(disp.innerHTML);
-    operatorChoice = '/';
-    clicked = true;
-}
-
-opButt[4].addEventListener('click',getTotal)
-function getTotal(){
-    var res = Number(lastNum) + Number(disp.innerHTML)
-    if (operatorChoice == '+'){
-        disp.innerHTML = res;
-    } else if (operatorChoice == '-'){
-        var res = Number(lastNum) - Number(disp.innerHTML)
-        disp.innerHTML = res;
-    } else if (operatorChoice == '*'){
-        var res = Number(lastNum) * Number(disp.innerHTML)
-        disp.innerHTML = res;
-    } else if (operatorChoice == '/'){
-        var res = Number(lastNum) / Number(disp.innerHTML)
-        disp.innerHTML = res;
-    } else {
-        throw error
+disp.addEventListener('click',secret1)
+function secret1(){
+    for (var i = 0; i < disp.innerHTML.length;i++){
+        if (disp.innerHTML[i] == 1 && disp.innerHTML[i+1] == 7){
+            alert('FOUND SECRET 1 OF 17!')
+        }
     }
-    operatorChoice = null;
 }
 
+//Set interval to check clicked value;
 window.setInterval(function(){
-    console.log(typeof disp.innerHTML);
     console.log(clicked)
 }, 5000)
